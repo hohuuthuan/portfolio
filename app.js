240c939f014919fb73afc3ba0198401b09ea2e9d
Huu Thuan <hohuuthuan789@gmail.com>
2026-01-19 04:09:05 +0700

    feat(hero): add extra 50px top spacing below navbar

--- app.js
+++ app.js
@@ -194,7 +194,7 @@
 
     const setNavHeight = function () {
       const height = Math.ceil(nav.getBoundingClientRect().height);
-      const extraOffset = 100;
+      const extraOffset = 50;
 
       document.documentElement.style.setProperty(
         "--nav-h",
